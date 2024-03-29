fca0d4493c56d02ebdb796b339864c4b21cb3d1a
temitopeohassan <temitope.hassan.2@gmail.com>
2024-03-29 11:05:26 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Connect Subgraph To A React Frontend</h1>
+      </div>
+    </>
+  )
+}
+
+export default App;
